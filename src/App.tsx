d55ccd6275236004a6f77cd7aae271081f99abce
Pete Gadomski <pete.gadomski@gmail.com>
2025-06-18 13:31:13 -0600

    feat: add color mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import "./App.css";
 import Map from "./components/map";
+import Overlay from "./components/overlay";
 
 function App() {
   return (
     <>
+      <Overlay></Overlay>
       <Map></Map>
     </>
   );
--- src/components/map.tsx
+++ src/components/map.tsx
@@ -1,23 +1,28 @@
 import "maplibre-gl/dist/maplibre-gl.css";
 import { useRef } from "react";
 import { type MapRef, Map as MaplibreMap } from "react-map-gl/maplibre";
+import { useColorModeValue } from "./ui/color-mode";
 
 export default function Map() {
   const mapRef = useRef<MapRef>(null);
+  const mapStyle = useColorModeValue(
+    "positron-gl-style",
+    "dark-matter-gl-style"
+  );
 
   return (
     <MaplibreMap
       ref={mapRef}
       initialViewState={{
-        longitude: -105.1019,
-        latitude: 40.1672,
-        zoom: 8,
+        longitude: 0,
+        latitude: 0,
+        zoom: 1,
       }}
       style={{
         height: "100vh",
         width: "100vw",
       }}
-      mapStyle="https://basemaps.cartocdn.com/gl/positron-gl-style/style.json"
+      mapStyle={`https://basemaps.cartocdn.com/gl/${mapStyle}/style.json`}
     ></MaplibreMap>
   );
 }
