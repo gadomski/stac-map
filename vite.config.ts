5379802c080d37583d3bc17ae04e45c19fbd8d76
Pete Gadomski <pete.gadomski@gmail.com>
2025-06-20 12:45:44 -0600

    fix: vite config

--- vite.config.ts
+++ vite.config.ts
@@ -6,5 +6,6 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/stac-geoparquet-viewer/",
   plugins: [react(), tsconfigPaths(), wasm(), topLevelAwait()],
 });
