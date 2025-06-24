1dca8e9d0c55e8e2e614cbee4a4c74aab5ccd2ba
Pete Gadomski <pete.gadomski@gmail.com>
2025-06-24 05:04:40 -0600

    refactor: remove overlay component

--- src/app.tsx
+++ src/app.tsx
@@ -1,10 +1,9 @@
-import { Box, useFileUpload } from "@chakra-ui/react";
+import { Box, Container, useFileUpload } from "@chakra-ui/react";
 import { useEffect, useState } from "react";
 import "./app.css";
 import Header from "./components/header";
 import { Map } from "./components/map";
 import { LayersProvider } from "./components/map/provider";
-import Overlay from "./components/overlay";
 import { Panel } from "./components/panel";
 import { isUrl } from "./components/stac/utils";
 import { Toaster } from "./components/ui/toaster";
@@ -40,10 +39,10 @@ export default function App() {
         <Map></Map>
       </Box>
       <Box zIndex={1}>
-        <Overlay>
+        <Container fluid h={"dvh"} pointerEvents={"none"}>
           <Header href={href} setHref={setHref}></Header>
           <Panel href={href} setHref={setHref} fileUpload={fileUpload}></Panel>
-        </Overlay>
+        </Container>
       </Box>
       <Toaster></Toaster>
     </LayersProvider>
