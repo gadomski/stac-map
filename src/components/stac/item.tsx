f4ea6cf4d1cd97a6583dfa981c96430375cc2060
Pete Gadomski <pete.gadomski@gmail.com>
2025-06-25 05:22:13 -0600

    feat: add zoom button

--- src/components/stac/item.tsx
+++ src/components/stac/item.tsx
@@ -1,7 +1,7 @@
-import { SimpleGrid, Stack } from "@chakra-ui/react";
+import { HStack, IconButton, SimpleGrid, Stack } from "@chakra-ui/react";
 import { GeoJsonLayer } from "@deck.gl/layers";
 import { useEffect } from "react";
-import { LuFile } from "react-icons/lu";
+import { LuFile, LuMaximize } from "react-icons/lu";
 import type { StacItem } from "stac-ts";
 import { useMapDispatch } from "../map/context";
 import { AssetCard } from "./asset";
@@ -20,7 +20,10 @@ export function Item({ item }: { item: StacItem }) {
         filled: true,
         getFillColor: [207, 63, 2, 100],
       });
-      dispatch({ type: "set-layers", layers: [layer], bbox: item.bbox });
+      dispatch({ type: "set-layers", layers: [layer] });
+      if (item.bbox) {
+        dispatch({ type: "set-fit-bbox", bbox: item.bbox });
+      }
     }
   }, [item, dispatch]);
 
@@ -32,6 +35,19 @@ export function Item({ item }: { item: StacItem }) {
         id={item.id}
         icon={<LuFile></LuFile>}
       ></ValueInfo>
+
+      <HStack>
+        <IconButton
+          size={"xs"}
+          variant={"surface"}
+          disabled={item.bbox === undefined}
+          onClick={() =>
+            item.bbox && dispatch({ type: "set-fit-bbox", bbox: item.bbox })
+          }
+        >
+          <LuMaximize></LuMaximize>
+        </IconButton>
+      </HStack>
 
       <SimpleGrid columns={{ base: 1, md: 2 }} gap={4}>
         {Object.entries(item.assets).map(([key, asset]) => (
