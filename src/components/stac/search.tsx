bab2b391ecaa001d52ab934c1ea1e5e3f937b9dc
Pete Gadomski <pete.gadomski@gmail.com>
2025-06-25 06:09:22 -0600

    fix: filter collections on search

--- src/components/stac/search.tsx
+++ src/components/stac/search.tsx
@@ -1,4 +1,12 @@
-import { DataList } from "@chakra-ui/react";
+import {
+  Badge,
+  createListCollection,
+  DataList,
+  Portal,
+  Select,
+} from "@chakra-ui/react";
+import { LngLatBounds } from "maplibre-gl";
+import { useEffect, useState } from "react";
 import type { StacCollection, StacLink } from "stac-ts";
 import { useMap } from "../map/context";
 import { InfoTip } from "../ui/toggle-tip";
@@ -11,6 +19,17 @@ export default function Search({
   links: StacLink[];
 }) {
   const { bounds } = useMap();
+  const [filteredCollections, setFilteredCollections] = useState(collections);
+
+  useEffect(() => {
+    if (bounds) {
+      setFilteredCollections(
+        collections.filter((collection) =>
+          isCollectionWithinBounds(collection, bounds)
+        )
+      );
+    }
+  }, [collections, setFilteredCollections, bounds]);
 
   return (
     <DataList.Root>
@@ -27,6 +46,70 @@ export default function Search({
             .join(", ")}
         </DataList.ItemValue>
       </DataList.Item>
+      <DataList.Item>
+        <DataList.ItemLabel>
+          Collections <Badge>{filteredCollections.length}</Badge>
+          <InfoTip content="Filtered to viewport"></InfoTip>
+        </DataList.ItemLabel>
+        <DataList.ItemValue>
+          <CollectionsSelect
+            collections={filteredCollections}
+          ></CollectionsSelect>
+        </DataList.ItemValue>
+      </DataList.Item>
     </DataList.Root>
+  );
+}
+
+function CollectionsSelect({ collections }: { collections: StacCollection[] }) {
+  const collection = createListCollection({
+    items: collections,
+    itemToString: (collection) => collection.title || collection.id,
+    itemToValue: (collection) => collection.id,
+  });
+  return (
+    <Select.Root multiple collection={collection}>
+      <Select.HiddenSelect />
+      <Select.Control>
+        <Select.Trigger>
+          <Select.ValueText placeholder="Select collection(s)" />
+        </Select.Trigger>
+        <Select.IndicatorGroup>
+          <Select.Indicator />
+        </Select.IndicatorGroup>
+      </Select.Control>
+      <Portal>
+        <Select.Positioner>
+          <Select.Content>
+            {collection.items.map((collection) => (
+              <Select.Item item={collection} key={collection.id}>
+                {collection.title || collection.id}
+                <Select.ItemIndicator />
+              </Select.Item>
+            ))}
+          </Select.Content>
+        </Select.Positioner>
+      </Portal>
+    </Select.Root>
+  );
+}
+
+function isCollectionWithinBounds(
+  collection: StacCollection,
+  bounds: LngLatBounds
+) {
+  const bbox = collection.extent.spatial.bbox[0];
+  let collectionBounds;
+  if (bbox.length == 4) {
+    collectionBounds = [bbox[0], bbox[1], bbox[2], bbox[3]];
+  } else {
+    // assume 6
+    collectionBounds = [bbox[0], bbox[1], bbox[3], bbox[4]];
+  }
+  return (
+    collectionBounds[0] <= bounds.getEast() &&
+    collectionBounds[2] >= bounds.getWest() &&
+    collectionBounds[1] <= bounds.getNorth() &&
+    collectionBounds[3] >= bounds.getSouth()
   );
 }
