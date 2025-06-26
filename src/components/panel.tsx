343e5656fbaf92cf0475261fbbca464eccc811d2
Pete Gadomski <pete.gadomski@gmail.com>
2025-06-26 08:53:40 -0600

    fix: some stuff

--- src/components/panel.tsx
+++ src/components/panel.tsx
@@ -1,8 +1,11 @@
 import {
   Box,
+  Button,
   FileUpload,
+  HStack,
   Icon,
   SimpleGrid,
+  Stack,
   Tabs,
   type UseFileUploadReturn,
 } from "@chakra-ui/react";
@@ -128,11 +131,25 @@ export function Panel({
               setItemCollection={setSearch}
             ></Search>
             {search && (
-              <ItemCollection
-                itemCollection={search}
-                setLayers={setSearchLayers}
-                setPicked={setPicked}
-              ></ItemCollection>
+              <Stack gap={4}>
+                <ItemCollection
+                  itemCollection={search}
+                  setLayers={setSearchLayers}
+                  setPicked={setPicked}
+                ></ItemCollection>
+                <HStack>
+                  <Button
+                    variant={"surface"}
+                    size={"sm"}
+                    onClick={() => {
+                      setSearch(undefined);
+                      setSearchLayers([]);
+                    }}
+                  >
+                    Clear search results
+                  </Button>
+                </HStack>
+              </Stack>
             )}
           </Tabs.Content>
           <Tabs.Content value="picked">
